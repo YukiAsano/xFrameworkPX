6b9adfaeab3aadce5dc48f78ce6711a490e2bdd2
Yuki Asano <asano@xenophy.com>
2014-05-02 13:33:03 +0900

    bootstrap修正

--- public_html/bootstrap.php
+++ public_html/bootstrap.php
@@ -7,15 +7,30 @@ set_include_path('../library/' . PATH_SEPARATOR . get_include_path());
 // }}}
 // {{{ Include xFrameworPX
 
+require_once 'xFrameworkPX/Yaml.php';
+require_once 'xFrameworkPX/Master.php';
+require_once 'xFrameworkPX/Environment.php';
+
 include '../locales/ja.php';
 include 'xFrameworkPX/Loader/Core.php';
 
 // }}}
 // {{{ xFrameworkPX Run
 
+$bForce = false;
+
+// メンテナンスページが設定されていたら、強制的にコントローラを実行させる
+//$bForce = (xFrameworkPX_Environment::getInstance()->get('maintenance') == '1');
+
+//if ($bForce == false) {
+// プレサイトページが設定されていたら、強制的にコントローラを実行させる
+//$bForce = (xFrameworkPX_Environment::getInstance()->get('presite') == '1');
+//}
+
 xFrameworkPX_Dispatcher::getInstance()->run(
     array(
-        'DEBUG' => 2
+        'DEBUG' => 2,
+        'FORCE_CONTROLLER_EXECUTE' => $bForce,
     )
 );
 
